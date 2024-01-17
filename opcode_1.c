00d87461d1b7ca6571a6c0940547a92065dd91d8
RamzyAR7 <rameya683@gmail.com>
2024-01-18 01:04:51 +0200

    add ramzy

--- opcode_1.c
+++ opcode_1.c
@@ -3,7 +3,7 @@
  * add_opcode - adds the top two elements of the stack
  * @stack: stack
  * @line_number: line number
-*/
+ */
 void add_opcode(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
@@ -39,7 +39,7 @@ void nop_opcode(stack_t **stack, unsigned int line_number)
  * free_stack - free stack
  * @stack: stack
  * Return: void
-*/
+ */
 void free_stack(stack_t *stack)
 {
 	stack_t *tmp = stack;
@@ -55,7 +55,7 @@ void free_stack(stack_t *stack)
  * sub_opcode - subtracts the top of the stack from the second top of the stack
  * @stack: stack
  * @line_number: line number
-*/
+ */
 void sub_opcode(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
@@ -86,23 +86,29 @@ void swap_opcode(stack_t **stack, unsigned int line_number)
 	stack_t *first;
 	stack_t *second;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack && (*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
-		free_stack(*stack);
+		first = *stack;
+		second = first->next;
+		first->next = second->next;
+		if (second->next != NULL)
+		{
+			second->next->prev = first;
+		}
+
+		second->prev = NULL;
+		second->next = first;
+		first->prev = second;
+
+		*stack = second;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		if (*stack)
+		{
+			free_stack(*stack);
+		}
 		exit(EXIT_FAILURE);
 	}
-	first = *stack;
-	second = first->next;
-	first->next = second->next;
-	if (second->next != NULL)
-	{
-		second->next->prev = first;
-	}
-
-	second->prev = NULL;
-	second->next = first;
-	first->prev = second;
-
-	*stack = second;
 }
